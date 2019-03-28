8d57994bb43361ffc9f3ed14b5cd83edadf0d96d
AndrewBraun <ABandrewbraun@gmail.com>
2019-03-28 12:45:17 -0700

    Update uart.c

--- uart/uart.c
+++ uart/uart.c
@@ -8,6 +8,8 @@
  */
 #include "uart.h"
 
+#define F_CPU 8000000UL
+
 #ifndef F_CPU
 #warning "F_CPU not defined for uart.c."
 #define F_CPU 11059200UL
@@ -22,57 +24,77 @@ static volatile uint8_t uart_buffer_index;
  */
 void uart_init(UART_BPS bitrate)
 {
-	UCSR0A = _BV(U2X1);
-	UCSR0B = _BV(RXEN0) | _BV(TXEN0) | _BV(RXCIE0);
-	UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
+	//UCSR1A = (1 << U2X1);
+	UCSR1B = (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0);
+	UCSR1C = (1 << UCSZ01) | (1 << UCSZ00);
 
 	UBRR0H = 0;	// for any speed >= 9600 bps, the UBBR value fits in the low byte.
+	UBRR1H = 0;
 
 	// See the appropriate AVR hardware specification for a table of UBBR values at different
 	// clock speeds.
 	switch (bitrate)
 	{
 #if F_CPU==8000000UL
+	case UART_9600:
+		UBRR0L = 104;
+		UBRR1L = 208;
 	case UART_19200:
 		UBRR0L = 51;
+		UBRR1L = 102;
 		break;
 	case UART_38400:
 		UBRR0L = 25;
+		UBRR1L = 50;
 		break;
 	case UART_57600:
 		UBRR0L = 16;
+		UBRR1L = 32;
 		break;
 	default:
 		UBRR0L = 51;
+		UBRR1L = 102;
 #elif F_CPU==16000000UL
+	case UART_9600:
+		UBRR0L = 208;
+		UBRR1L = 416;
 	case UART_19200:
 		UBRR0L = 103;
+		UBRR1L = 206;
 		break;
 	case UART_38400:
 		UBRR0L = 51;
+		UBRR1L = 102;
 		break;
 	case UART_57600:
 		UBRR0L = 34;
+		UBRR1L = 68;
 		break;
 	default:
 		UBRR0L = 103;
+		UBRR1L = 206;
 #elif F_CPU==18432000UL
 	case UART_19200:
 		UBRR0L = 119;
+		UBRR1L = 238;
 		break;
 	case UART_38400:
 		UBRR0L = 59;
+		UBRR1L = 118;
 		break;
 	case UART_57600:
 		UBRR0L = 39;
+		UBRR1L = 78;
 		break;
 	default:
 		UBRR0L = 119;
+		UBRR1L = 238;
 		break;
 #else
 #warning "F_CPU undefined or not supported in uart.c."
 	default:
 		UBRR0L = 71;
+		UBRR1L = 142;
 		break;
 #endif
 	}
@@ -86,13 +108,21 @@ void uart_init(UART_BPS bitrate)
  *
  * @param byte data to trasmit
  */
-void uart_putchar(uint8_t byte)
+void uart_putchar_0(uint8_t byte)
 {
     /* wait for empty transmit buffer */
     while (!( UCSR0A & (1 << UDRE0)));
 
     /* Put data into buffer, sends the data */
     UDR0 = byte;
+}
+
+void uart_putchar_1(uint8_t byte){
+	/* wait for empty transmit buffer */
+	while (!( UCSR1A & (1 << UDRE1)));
+
+	/* Put data into buffer, sends the data */
+	UDR1 = byte;
 }
 
 /**
@@ -139,8 +169,3 @@ ISR(USART0_RX_vect)
     uart_buffer[uart_buffer_index] = UDR0;
     uart_buffer_index = (uart_buffer_index + 1) % UART_BUFFER_SIZE;
 }
-
-
-
-
-
