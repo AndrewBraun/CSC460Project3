569553e6e4acb36189428fad2a657303fcd13027
AndrewBraun <ABandrewbraun@gmail.com>
2019-04-04 14:55:13 -0700

    Added servo code

--- servo/servo.c
+++ servo/servo.c
@@ -5,16 +5,40 @@
  *  Author: Andrew
  */
 #include <stdio.h>
+#include <stdlib.h>
 #include "servo.h"
+#include "../joystick/joystick.h"
+#include "../message/message.h"
+#include "../uart/uart.h"
+
+void send_servo_joystick_task(void* param_ptr){
+	DDRB = 0xFF;
+	PORTB = 0x00;
+	
+	int16_t x_value = ((int16_t) (servo_joystick.x_value)) - 127;
+	int16_t y_value = ((int16_t) (servo_joystick.y_value)) - 127;
+	
+	int8_t pan_velocity = 0;
+	int8_t tilt_velocity = 0;
+		
+	// Deadband
+	if (abs(x_value) >= 50 || abs(y_value) >= 50) {
+		pan_velocity = x_value > 0 ? 1 : -1;
+		tilt_velocity = y_value > 0 ? 1 : -1;
+	}
+	
+	uart_putchar(UART_1, Cmd_MoveServos);
+	uart_putchar(UART_1, pan_velocity);
+	uart_putchar(UART_1, tilt_velocity);
+}
 
 void servo_init(Servo* servo, uint8_t pin){
 	servo->pin = pin;
+	servo->position = 90;
+	servo->velocity = 0;
 }
 
-void update_servo(Servo* servo, uint8_t new_period){
-	
-}
-
-void servo_change_state(Servo* servo){
+void Task_UpdateServo(void* servo_arg){
+	Servo* servo = (Servo *) servo_arg;
 	
 }
--- servo/servo.h
+++ servo/servo.h
@@ -9,23 +9,39 @@
 #ifndef SERVO_H_
 #define SERVO_H_
 
-typedef enum {
-	HIGH,
-	LOW,
-} servo_state;
-
+/*
+ * Servo struct used by the Roomba Arduino.
+ * Pin is the signal pin for the servo motor.
+ * Position is the servo's current position from 0 to 180.
+ * Velocity is the direction the servo will move when update_servo_task is called.
+ * If velocity = 1, position will be incremented up to 180
+ * If veloctiy = -1, position will be decremented down to 0.
+ * If velocity = 0, position will not be changed.
+ */
 typedef struct servo{
 	uint8_t pin;
-	uint8_t on_time;
-	servo_state;
+	uint8_t position;
+	int8_t velocity;
 } Servo;
 
 Servo pan_servo, tilt_servo;
 
+/*
+ * Method for the controller to write the servo movement to the other Arduino.
+ */
+void send_servo_joystick_task(void* param_ptr);
+
+/*
+ * Method for Roomba Arduino to initialize a servo motor.
+ */
 void servo_init(Servo* servo, uint8_t pin);
 
-void update_servo(Servo* servo, uint8_t new_period);
-
-void servo_change_state(Servo* servo);
+/*
+ * Changes the position of the servo motor.
+ * If new_direction = 1, then it increments the position up to 180.
+ * If new_direction = -1, it decrements the position down to 0.
+ * If new_direction = 0, the position does not change.
+ */
+void Task_UpdateServo(void* servo_arg);
 
 #endif /* SERVO_H_ */
